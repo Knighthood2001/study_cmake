2e64245c19563eef7a2e412c67fb3fe392409b8e
knighthood <2109695291@qq.com>
2025-02-25 21:29:14 +0800

    带有build

---
+++ demo05_2/build/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/wu/code/study_cmake/demo05_2/src/main.c" "/home/wu/code/study_cmake/demo05_2/build/CMakeFiles/main.dir/src/main.c.o"
+  "/home/wu/code/study_cmake/demo05_2/src/testFunc.c" "/home/wu/code/study_cmake/demo05_2/build/CMakeFiles/main.dir/src/testFunc.c.o"
+  "/home/wu/code/study_cmake/demo05_2/src/testFunc1.c" "/home/wu/code/study_cmake/demo05_2/build/CMakeFiles/main.dir/src/testFunc1.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
